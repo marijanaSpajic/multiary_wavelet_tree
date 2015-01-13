7f3266ae7dd225d56b60cfcb66ee1657413584d1
nikola-munder <nikola.munder@fer.hr>
2015-01-13 19:29:29 +0100

    perl, ucitavanje datoteke
    ucitavanje datoteke i zatvaranje

--- perl/mwt.pl
+++ perl/mwt.pl
@@ -1,2 +1,10 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
 
+my $name = $ARGV[0];                      #file specified in argument
+open (my $file, '<', $name) or die $!;    #open file with given filename
+
+
+print <$file>;
+close($file);                             #close the input file
