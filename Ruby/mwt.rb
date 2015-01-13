5f9955255fc7f977860b2d67495a5926558642cd
marg- <maja.buljan.42@gmail.com>
2015-01-14 00:07:20 +0100

    convert to base n

--- Ruby/mwt.rb
+++ Ruby/mwt.rb
@@ -1,6 +1,9 @@
-arity = ARGV[0]
+#arity = ARGV[0]
 #filepath = ARGV[1]
+arity = "4"
 filepath = "../generate_inputs/inputs/input_3"
+
+arity = arity.to_i
 
 # assuming that the input file is a FASTA file
 
@@ -16,6 +19,19 @@ f.each_line do |line|
 end
 f.close
 
+alphabet = sequence.split(//).uniq
+
+alphabet_ascii = []
+
+for character in alphabet do
+	alphabet_ascii << character.ord
+	end
+
+alphabet_basen = []
+
+for character in alphabet_ascii do
+	alphabet_basen << character.to_s(arity)
+	end
+
 print "\n"
-print sequence
-print "\n"
+print alphabet_basen
