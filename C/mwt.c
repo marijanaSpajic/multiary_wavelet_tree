6acdd6ad901755628159d1140ddbed530d9da57b
Jelena Konfic <jelena.konfic@gmail.com>
2015-01-14 01:36:12 +0100

    Changed saving of alphabet, coding characters of aplhabet (number base converter)

--- C/mwt.c
+++ C/mwt.c
@@ -2,13 +2,39 @@
 #include<stdlib.h>
 #include<math.h>
 
+//convert number to different base; result is code value of alphabet character
+int * calculateCodeValue (int value, int base, int numOfDigits){
+    int *codeValue = (int *) malloc(numOfDigits*sizeof(int));
+
+    if(base==10 || value==0){
+        codeValue[numOfDigits - 1] = value;
+    }
+    else {
+        int result = value;
+        int modResult = 0;
+        int i = 1;
+        while(result != 0){
+            modResult = result % base;
+            codeValue[numOfDigits - i] = modResult;
+            i++;
+            result = result/base;
+        }
+        while(i < (numOfDigits+1)){
+            codeValue[numOfDigits - i] = 0;
+            i++;
+        }
+    }
+
+    return codeValue;
+}
+
 int main (int argc, char *argv[]){
     if(argc != 3){
         printf("Number of parameters should be 2 (filename, arity).");
         exit(1);
     }
 
-    int alphabet[128] = {0};
+    int * alphabet[128] = {NULL};
     int arity = atoi(argv[2]);
     int treeLayers = ceil(7/log2(arity));
     printf("Number of layers: %d \n", treeLayers);
@@ -32,26 +58,22 @@ int main (int argc, char *argv[]){
     int numOfChar = 0;
     while((character = fgetc(file)) != EOF){
             inputStream[i] = character;
-            if(alphabet[character]==0){
-                alphabet[character]=1;
-                numOfChar++;
+            if(alphabet[character]==NULL){
+                alphabet[character] = calculateCodeValue(character, arity, treeLayers);
             }
             i++;
             }
 
-    char charOfAlphabet[numOfChar];
     int j;
     int k = 0;
     for (j = 0; j < 128; j++){
-        if(alphabet[j]==1){
-            charOfAlphabet[k] = j;
-            k++;
+        if(alphabet[j] != NULL){
+            for(k = 0; k < treeLayers; k++){
+                printf("%d", alphabet[j][k]);
+            }
+        printf("\n");
         }
     }
-    //for(j=0; j < fileLength; j++){
-      //  int a = inputStream[j];
-        //printf("Znak: %d \n", a);
-    //}
 
     fclose(file);
 }
