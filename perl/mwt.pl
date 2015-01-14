2a391591f360d22a546358eb5939b8a1827c8bf2
nikola-munder <nikola.munder@fer.hr>
2015-01-14 15:06:39 +0100

    adding zeros in numbers for layers
    adding zeros in front if the number doesn't have enough digits

--- perl/mwt.pl
+++ perl/mwt.pl
@@ -3,12 +3,13 @@ use strict;
 use warnings;
 use POSIX;
 
-
+#log in base 2
 sub log2 {
 	my $n = shift;
 	return log($n)/log(2);
 }
 
+#calculating number of layers with given formula
 sub get_num_layers{
 	my $n = shift;
 	return ceil(7/log2($n));
@@ -19,6 +20,7 @@ sub get_num_layers{
 sub convert_to_base{
 	my $num = shift;
 	my $base = shift;
+	my $num_layers = shift;
 	my $digit = 0;
 	my @converted = ();
 	
@@ -28,18 +30,26 @@ sub convert_to_base{
 		$num = floor($num / $base);
 	}
 	
+	#adding zeros if number has too few digits
+	my $size_converted = @converted; 
+	while ($size_converted < $num_layers){
+		unshift (@converted, 0);
+		$size_converted = $size_converted + 1;
+	}
+	
+	
 	return @converted;
 }
 
-my $name = $ARGV[0];                      #file specified in argument
-open (my $file, '<', $name) or die $!;    #open file with given filename
+##my $name = $ARGV[0];                      #file specified in argument
+##open (my $file, '<', $name) or die $!;    #open file with given filename
 
 
 
-#my $proba = get_num_layers(5);           testing - get_num_layers
-my @proba = convert_to_base(67, 4);      testing - convert_to_base
+#my $proba = get_num_layers(5);           #testing - get_num_layers
+my @proba = convert_to_base(67, 4, 5);    #testing - convert_to_base
 print "@proba";
 
-#print <$file>;                           testing - reading the file
+#print <$file>;                           #testing - reading the file
 
-close($file);                             #close the input file
+##close($file);                             #close the input file
