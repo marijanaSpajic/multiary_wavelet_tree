7fa489dac28b492ff32583df582fde4a9cd1cdab
marg- <maja.buljan.42@gmail.com>
2015-01-14 13:26:38 +0100

    created layers

--- Ruby/mwt.rb
+++ Ruby/mwt.rb
@@ -11,7 +11,7 @@ base = 7/base
 base = base.ceil
 base = base.to_i
 
-# assuming that the input file is a FASTA file
+# reading input file, assuming that the input is a FASTA file
 
 sequence = ""
 
@@ -24,6 +24,9 @@ f.each_line do |line|
 	end
 end
 f.close
+
+
+# get alphabet
 
 alphabet = sequence.split(//).uniq
 
@@ -39,7 +42,13 @@ for character in alphabet_ascii do
 	alphabet_basen << character.to_s(base)
 	end
 
+
+# translation dictionary from original alphabet to base_n
+
 transformation = { alphabet[0] => alphabet_basen[0], alphabet[1] => alphabet_basen[1], alphabet[2] => alphabet_basen[2], alphabet[3] => alphabet_basen[3] }
+
+
+# translating the input sequence
 
 sequence_transformed = []
 
@@ -49,6 +58,32 @@ while i<sequence.length do
 	sequence_transformed << transformation[sequence[i]]
 	i+=1
 	end
+
+
+# building layers
+# base = number of layers
+# layer array = [ [layer lvl_n], [layers lvl_n-1], layers lvl_n-2, ... , layers lvl_1 ]
+
+
+
+
+layers = []
+
+i = 0
+while i<base do
+	layers << []
+	i+=1
+	end
+
+i=0
+while i<base-1 do
+	layers[i+1] << layers[i]
+	layers[i+1] << layers[i]
+	layers[i+1] << layers[i]
+	layers[i+1] << layers[i]
+	i+=1
+	end
+
 
 
 
