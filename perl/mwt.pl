5dde7d49bea674307d58d5c7e30dceeaa1eeb138
nikola-munder <nikola.munder@fer.hr>
2015-01-14 12:14:03 +0100

    converting numbers from base 10 to others

--- perl/mwt.pl
+++ perl/mwt.pl
@@ -15,12 +15,33 @@ sub get_num_layers{
 }
 
 
+#converting number from base 10 to wanted base by taking modulus division and division
+sub convert_to_base{
+	my $num = shift;
+	my $base = shift;
+	my $place = 0;
+	my $digit = 0;
+	my $converted = 0;
+	
+	while ($num > 0){
+		$digit = $num % $base;
+		$converted = $converted + $digit*(10 ** $place);    #numbers must go from back to front, ** is exp
+		$place = $place + 1;
+		$num = floor($num / $base);
+	}
+	
+	return $converted;
+}
 
 my $name = $ARGV[0];                      #file specified in argument
 open (my $file, '<', $name) or die $!;    #open file with given filename
 
+
+
 #my $proba = get_num_layers(5);           testing - get_num_layers
+#my $proba = convert_to_base(67, 4);      testing - convert_to_base
 #print $proba;
 
 #print <$file>;                           testing - reading the file
+
 close($file);                             #close the input file
